1a6ff2d77f3fe954c49f836fb42967cccc0fae42
Can <63351166+fastuptime@users.noreply.github.com>
2022-08-23 03:06:00 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 def log(log_text):
     log_text = str(time.strftime("%Y.%m.%d %H:%M:%S")) + " ➾ " + log_text
@@ -20,24 +21,30 @@ vice_url = input("Video Linki: ") # Url
 try:
     driver.get(vice_url)
     time.sleep(5)
-    html = driver.find_element_by_tag_name('html')
+    html = driver.find_element(By.TAG_NAME, "html")
+    html.send_keys(Keys.END)
+    time.sleep(1)
+    html.send_keys(Keys.END)
+    time.sleep(1)
     html.send_keys(Keys.END)
     time.sleep(3)
-    kac_yorum_var = driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[1]/ytd-comments-header-renderer/div[1]/h2/yt-formatted-string/span[1]').text
+    kac_yorum_var = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-comments/ytd-item-section-renderer/div[1]/ytd-comments-header-renderer/div[1]/h2/yt-formatted-string/span[1]").text
     #. var ise sil
     kac_yorum_var = kac_yorum_var.replace(".", "")
     log('Toplam ' + kac_yorum_var + ' yorum var.')
     for i in range(int(kac_yorum_var)):
         try:
-            yorum = driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[' + str(i) + ']/ytd-comment-renderer/div[3]/div[2]/ytd-expander/div').text
-            yorum_sahibi = driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[' + str(i) + ']/ytd-comment-renderer/div[3]/div[2]/div/div[2]/h3/a/span').text
+            #/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[1]/ytd-comment-renderer/div[3]/div[2]/div[1]/div[2]/h3/a/span
+            #/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[
+            yorum = driver.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[' + str(i) + ']/ytd-comment-renderer/div[3]/div[2]/div[2]/ytd-expander/div/yt-formatted-string').text
+            yorum_sahibi = driver.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[' + str(i) + ']/ytd-comment-renderer/div[3]/div[2]/div[1]/div[2]/h3/a/span').text
             yorum_file = open("yorumlar.txt", "a", encoding='utf-8')
             log('Sahibi: ' + yorum_sahibi + ' Yorum: ' + yorum)
             #yorum boş ise geç
             if yorum == "":
                 continue
             yorum_file.write('Sahibi: ' + yorum_sahibi + ' Yorum: ' + yorum + '\n')
-            html = driver.find_element_by_tag_name('html')
+            html = driver.find_element(By.TAG_NAME, "html")
             html.send_keys(Keys.END)
             time.sleep(global_delay)
         except:
